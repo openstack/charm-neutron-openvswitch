e3e8c74d99623ceb68558a387283cf0d19c45c58
Felipe Reyes <felipe.reyes@canonical.com>
2022-02-08 16:50:42 -0300

    Mark ovs-record-hostname as deferable service.
    Add ovs-record-hostname to the list of deferrable services, this service
    was SRU'ed into Ubuntu >= Focal, when it's not mark as deferrable on
    package upgrades will induce a restart of openvswitch-switch.
    
    Change-Id: I4fa3256b39e62e9df223ab40f17f1677a621f293
    Closes-Bug: #1923636
    Related-Bug: #1915829
    (cherry picked from commit f6a9487b05b08e5481d0792d825c98548621e5c0)

--- hooks/neutron_ovs_utils.py
+++ hooks/neutron_ovs_utils.py
@@ -520,7 +520,7 @@ def deferrable_services():
 
     _svcs = services()
     _svcs.extend(['ovs-vswitchd', 'ovsdb-server', 'ovs-vswitchd-dpdk',
-                  'openvswitch-switch'])
+                  'openvswitch-switch', 'ovs-record-hostname'])
     return list(set(_svcs))
 
 
